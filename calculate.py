7c53fa7c35f2a66441f4998db6e2a528a0064001
GISELLE CHEN <makitru0115@gmail.com>
2021-06-27 18:27:23 +0800

    filter

--- calculate.py
+++ calculate.py
@@ -11,10 +11,34 @@ for d in data:
     sum_len = sum_len + len(d) #每次讀取一行獲得總數厚再存回去再抓第二行
 print('留言平均長度為',sum_len/len(data))    #獲得平均長度
 
+
+#篩選功能1
 new = []     
 for d in data:       
     if len(d) < 100:
        new.append(d)
 print('一共有',len(new),'筆留言長度小於100')       
 print(new[0])
-print(new[1])   
+print(new[1]) 
+
+
+#篩選功能2
+good = []
+for d in data:
+    if 'good' in d:
+        good.append(d)
+print('一共有',len(good),'筆留言有提到good')
+print(good[0])
+print(good[10]) 
+
+#list compreehension
+good =[d for d in data if 'good' in d]  #第一個d = good.append(d)
+print(len(good))  
+print(good[0])
+
+great = [1 for d in data if 'great' in d] #把第一個用１塞進去
+print(great)
+
+bad =[ 'bad' in d for d in data]  #"bad in d，表示如果bad in d，會是是非題
+print(bad)
+ 
