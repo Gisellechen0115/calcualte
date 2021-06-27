b1a000cbacdb824ec25d263324f10d982700e36b
GISELLE CHEN <makitru0115@gmail.com>
2021-06-27 17:53:43 +0800

    average

---
+++ calculate.py
@@ -0,0 +1,20 @@
+data = []
+count = 0
+with open('reviews.txt','r') as f:
+    for line in f:      
+        data.append( line)
+        count += 1   #我希望每一千筆印一次
+        if count % 100000 == 0:
+            print(len(data))           
+sum_len = 0
+for d in data:
+    sum_len = sum_len + len(d) #每次讀取一行獲得總數厚再存回去再抓第二行
+print('留言平均長度為',sum_len/len(data))    #獲得平均長度
+
+new = []     
+for d in data:       
+    if len(d) < 100:
+       new.append(d)
+print('一共有',len(new),'筆留言長度小於100')       
+print(new[0])
+print(new[1])   
